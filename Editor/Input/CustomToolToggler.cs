a7e3c41a22f97e254d0b5296611888807843092e
Jafar <info@jrahmati.nl>
2021-06-03 20:40:24 +0200

    Removed unintended submenu for custom tool toggler

--- Editor/Input/CustomToolToggler.cs
+++ Editor/Input/CustomToolToggler.cs
@@ -5,7 +5,7 @@ namespace JRahmatiNL.Unity3D.CustomEditorTools.Input
     public class CustomToolToggler
     {
         private static Tool _previousToolMode;
-        [MenuItem("Edit/Toggle on/off custom tool %T", false, 5)]
+        [MenuItem("Edit/Toggle on or off custom tool %T", false, 5)]
         // Note: this only works after selecting the custom tool first in the Unity Editor!
         private static void ToggleCustomTool()
         {
